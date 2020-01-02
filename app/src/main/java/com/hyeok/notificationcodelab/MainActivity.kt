3615baa11f4b9557fd486d1ef51520eae60cbff3
최혁재 <gurwo1007@gmail.com>
2020-01-02 15:23:14 +0900

    Notification 터치 시 MainActivity로 이동하도록 PendingIntent 추가

--- app/src/main/java/com/hyeok/notificationcodelab/MainActivity.kt
+++ app/src/main/java/com/hyeok/notificationcodelab/MainActivity.kt
@@ -2,7 +2,9 @@ package com.hyeok.notificationcodelab
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -43,9 +45,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun buildNotificationBuilder(): NotificationCompat.Builder {
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            NOTIFICATION_ID,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+
         return NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
             .setContentTitle("You've been notified!")
             .setContentText("This is your notification text.")
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setAutoCancel(true)
             .setSmallIcon(R.drawable.ic_android)
     }
 
