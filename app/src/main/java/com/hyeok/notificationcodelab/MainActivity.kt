91158b81ed9c0a66d89a0117c06dba9d5ddd06d1
최혁재 <gurwo1007@gmail.com>
2020-01-02 14:43:10 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/hyeok/notificationcodelab/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.hyeok.notificationcodelab
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val PRIMARY_CHANNEL_ID = "primary_notification_channel"
+    private val notifyManager by lazy { getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID,
+                "Mascot Notification",
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                description = "Notification from Mascot"
+            }
+
+            notifyManager.createNotificationChannel(notificationChannel)
+        }
+    }
+}
